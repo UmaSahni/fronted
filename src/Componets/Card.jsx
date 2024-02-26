8d400b6453aa8dd0c9edcf1223e6586ad4e0791d
Uma Sahni <monkeytown64@gmail.com>
2024-02-26 19:05:47 +0530

    Products page

---
+++ src/Componets/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="p-8 light-border rounded-lg text-white bacground-color-gray border border-white border-2 w-80 ">
+      <img className="m-auto" src="/Images/4.png" alt="" />
+
+      <p className="text-center mb-4">Bita Marketplace</p>
+      <div className="border-top"></div>
+      <p className="text-center mt-4 ">
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+      </p>
+    </div>
+  );
+};
+
+export default Card;
--- src/Componets/Products.jsx
+++ src/Componets/Products.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Card from "./Card";
 
 const Products = () => {
   return (
     <div className="">
       <h1 className="text-white text-center text-5xl py-4 ">
-        <b>Our <span className="change-color" >Products</span> </b>
+        <b>
+          Our <span className="change-color">Products</span>{" "}
+        </b>
       </h1>
 
-      <div className="container-md" >
-        <p className="text-center text-white text-xl" >Pellentesque habitant morbi tristique senectus et netus et Pellentesque habitant morbi.</p>
+      <div className="container-md">
+        <p className="text-center text-white text-xl">
+          Pellentesque habitant morbi tristique senectus et netus et
+          Pellentesque habitant morbi.
+        </p>
+      </div>
+      <div className=" justify-center gap-20 container-md flex flex-wrap ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
